d413ab30f2ab245659eba70afa57b3a665e70616
Vika Shcherbanych <vika.shcherbanych@dev.pro>
2022-05-02 17:46:12 +0300

    update task to count the letters in the word

--- maxCharCount.js
+++ maxCharCount.js
@@ -4,9 +4,7 @@ const str1 = 'sdnf,sbfgdfsmhbgdfvbvhdsfv3435'
 const maxCharCount = (word) => {
     const newWord = word.toLowerCase();
     const letterNumber = newWord.split('').reduce((acc, el) => {
-        console.log(acc[el])
         acc[el] ? acc[el] = acc[el] +1 : acc[el] = 1
-        console.log(acc)
         return acc
     }, {})
     
