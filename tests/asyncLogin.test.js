0d89fd70a1021509ec5e682c1df1e0e5c8bf22e2
Vika Shcherbanych <vika.shcherbanych@dev.pro>
2022-05-23 14:35:53 +0300

    update asyncLogin test

--- tests/asyncLogin.test.js
+++ tests/asyncLogin.test.js
@@ -1,13 +1,12 @@
 const { login, showError, checkLogin } = require("../asyncLogin");
 
-const res = { body: { mail: "fd@gmail.com", password: "1vj6kdfh7vdj" } };
-const error = "TypeError: Cannot destructure property 'mail' of 'req.body' as it is undefined";
+const req = { body: { mail: "fd@gmail.com", password: "1vj6kdfh7vdj" } };
 const checkLoginMock = jest.fn(checkLogin);
 const showErrorMock = jest.fn(showError);
 
 describe("Login function", () => {
   test("should checkLogin function will called 1 time", async () => {
-    await login(res, checkLoginMock, showErrorMock);
+    await login(req, checkLoginMock, showErrorMock);
     expect(checkLoginMock).toBeCalledTimes(1);
   });
   test("should showErrorMock function will called 1 time", async () => {
@@ -15,7 +14,7 @@ describe("Login function", () => {
     expect(showErrorMock).toBeCalledTimes(1);
   });
   test("should get parametrs  mail and password and return true", () => {
-    expect(login(res, checkLoginMock, showErrorMock)).toBe(true);
+    expect(login(req, checkLoginMock, showErrorMock)).toBe(true);
   });
   test("should get only one parametr and return false", () => {
     expect(login({body: { mail: "fd@gmail.com"}}, checkLoginMock, showErrorMock)).toBe(false);
